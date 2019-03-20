56f15088ce10a6b0792cb1e8ca5efd95b026d03c
Bakir Karovic <bakir.karovic@gmail.com>
2019-03-20 21:56:06 +0100

    added spaces

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static int sumacifara(int broj) {
-        int suma=0;
+        int suma = 0;
         while(broj !=0 ){
             suma = suma + (broj % 10);
             broj = broj / 10;
@@ -17,12 +17,11 @@ public class Main {
         int broj;
         Scanner ulaz = new Scanner(System.in);
         System.out.println("Unesi broj: ");
-        broj=ulaz.nextInt();
-        for(int i = 1 ; i <= broj ; i++){
+        broj = ulaz.nextInt();
+        for(int i = 1; i <= broj; i++){
             if(i % sumacifara(i)==0){
                 System.out.println(i);
             }
         }
-
     }
 }
