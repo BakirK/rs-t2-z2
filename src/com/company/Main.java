84e98946974dcb280efda14c5112d4a14b8ed92c
BakirK <dalila.herh@gmail.com>
2019-03-20 10:24:48 +0100

    css

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static int sumacifara(int n) {
-	int s=0;
-	while(n!=0){
-	    s=s+n%10;
-	    n=n/10;
-    }
-	return s;
+    public static int sumacifara(int broj) {
+        int suma=0;
+        while(broj !=0 ){
+            suma = suma + (broj % 10);
+            broj = broj / 10;
+        }
+        return suma;
     }
 
     public static void main(String[] args){
-        int n;
+        int broj;
         Scanner ulaz=new Scanner(System.in);
         System.out.println("Unesi broj: ");
-        n=ulaz.nextInt();
-        for(int i=1;i<n;i++){
-            if((i%sumacifara(i))==0){
+        broj=ulaz.nextInt();
+        for(int i = 1 ; i <= broj ; i++){
+            if(i % sumacifara(i)==0){
                 System.out.println(i);
             }
         }
