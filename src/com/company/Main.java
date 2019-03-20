fe0ace4b12834af241e69b1a73eebd5d74876994
BakirK <dalila.herh@gmail.com>
2019-03-20 10:27:39 +0100

    added spaces

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
     public static void main(String[] args){
         int broj;
-        Scanner ulaz=new Scanner(System.in);
+        Scanner ulaz = new Scanner(System.in);
         System.out.println("Unesi broj: ");
         broj=ulaz.nextInt();
         for(int i = 1 ; i <= broj ; i++){
